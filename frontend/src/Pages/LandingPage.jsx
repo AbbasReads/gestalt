403f9922c3f23178dab1c18656a070624ac174e5
AbbasReads <abbasmehdi3727@gmail.com>
2025-06-07 13:27:30 +0530

    feat: implement styled Button component, enhance file upload functionality, and update chat and landing page interactions

--- frontend/src/Pages/LandingPage.jsx
+++ frontend/src/Pages/LandingPage.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect,useContext} from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { SocketContext } from '../main.jsx';
+import Button from '../components/Button.jsx';
 
 const LandingPage = () => {
   const [username, setUsername] = useState('');
@@ -23,21 +24,16 @@ const LandingPage = () => {
 
   return (
     <div className="min-h-screen bg-neutral-900 flex items-center justify-center px-4">
-      <div className="w-full max-w-sm bg-neutral-800 rounded-xl p-6 shadow-lg">
+      <div className="w-full justify-items-center max-w-sm bg-neutral-800 rounded-xl p-6 shadow-lg">
         <h1 className="text-white text-2xl font-semibold mb-6 text-center">Create Chat Session</h1>
-        {localStorage.getItem("username")!=='' || <input
+        {localStorage.getItem("username") !== '' || <input
           type="text"
           placeholder="Enter your username"
           className="w-full mb-4 px-4 py-2 rounded-lg bg-neutral-700 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
         />}
-        <button
-          onClick={handleCreateSession}
-          className="w-full py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
-        >
-          Create Session
-        </button>
+        <Button text="GO" handleClick={handleCreateSession} />
       </div>
     </div>
   );
