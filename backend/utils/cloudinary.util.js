4878f91d8446e905606c4c45dedd2350c24134e7
AbbasReads <abbasmehdi3727@gmail.com>
2025-06-16 16:59:44 +0530

    feat: streamline file upload process by removing unnecessary console logs, enhancing error handling, and ensuring sessionId is included in the request

--- backend/utils/cloudinary.util.js
+++ backend/utils/cloudinary.util.js
@@ -3,12 +3,6 @@ import fs from 'fs'
 import dotenv from "dotenv"
 dotenv.config();
 
-const extensionToResourceType = {
-    jpg: 'image', jpeg: 'image', png: 'image', gif: 'image', webp: 'image', bmp: 'image', tiff: 'image', svg: 'image',
-    mp4: 'video', mov: 'video', avi: 'video', flv: 'video', mkv: 'video', webm: 'video', mpeg: 'video',
-    pdf: 'raw', txt: 'raw', doc: 'raw', docx: 'raw', xls: 'raw', xlsx: 'raw', csv: 'raw', zip: 'raw', rar: 'raw'
-};
-
 // Configuration
 cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY,
@@ -36,29 +30,10 @@ const uploadOnCloudinary = async (localFilePath, sessionId) => {
     }
 };
 
-const deleteFile = async (publicId) => {
-    console.log("Trying to delete", publicId, "...")
-    const ext = publicId.split(".")[1].toLowerCase();
-    const resourceType = extensionToResourceType[ext] || 'image';
-    console.log(resourceType);
-    cloudinary.uploader
-        .destroy(
-            publicId.split(".")[0],
-            {
-                resource_type: resourceType,
-                invalidate: true
-            }
-        ).then(response => {
-            console.log(response);
-        })
-        .catch(error => {
-            console.log(error);
-        })
-}
-const deleteFolder=async(sessionId)=>{
-    cloudinary.api.delete_resources_by_prefix(sessionId+"/")
-    .catch(err=> console.log(err))
-    
+const deleteFolder = async (sessionId) => {
+    cloudinary.api.delete_resources_by_prefix(sessionId + "/")
+    .then(() => cloudinary.api.delete_folder(sessionId))
+    .catch(err => console.log(err));
 }
 
-export { uploadOnCloudinary, deleteFile,deleteFolder }
+export { uploadOnCloudinary, deleteFolder }
