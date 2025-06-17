9f4e89c04454fd558116878a9315637821d7e3af
AbbasReads <abbasmehdi3727@gmail.com>
2025-06-17 14:15:28 +0530

    update CORS configuration to allow specific origins

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ import {sessionRouter} from './routes/session.route.js'
 import cors from "cors"
 const app=express();
 app.use(cors({
-    origin:"*"
+    origin:["https://gestalt-ashy.vercel.app","http://localhost:5173"]
 }))
 // const router=express.Router();
 
